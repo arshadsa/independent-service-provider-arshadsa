48de8a15118bf0415e38d077dfb652a9ad5ddb2f
Arshad <arshad.syed.anwar@gmail.com>
2022-04-18 17:43:43 +0100

    header and footer page

---
+++ src/Shared/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2 className='text-primary text-center'>404 Not found!</h2>
+        </div>
+    );
+};
+
+export default NotFound;
